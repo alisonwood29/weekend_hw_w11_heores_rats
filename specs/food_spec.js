de1048805e186d692c34696f2f5c87f2434254b4
Alison Wood <alisonwood29@gmail.com>
2018-02-16 14:22:36 +0000

    passing test for food replenish value

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -7,12 +7,16 @@ describe('Food', function () {
   let food;
 
   beforeEach(function () {
-    food = new Food('chocolate');
+    food = new Food('chocolate', 20);
   });
 
   it('should have name', function () {
     assert.strictEqual(food.name, 'chocolate');
   });
 
+  it('should have a replenish value', function () {
+    assert.strictEqual(food.replenishValue, 20);
+  });
 
-})
+
+});
