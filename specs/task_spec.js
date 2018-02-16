e71efd0491cfb9a2042533e30ea78344a6d0b122
Alison Wood <alisonwood29@gmail.com>
2018-02-16 14:06:22 +0000

    passing test for task difficulty

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -6,11 +6,15 @@ describe('Task', function () {
   let task;
 
   beforeEach(function () {
-    task = new Task('save the world');
+    task = new Task('save the world', 10);
   })
 
   it('should have a description', function () {
-    assert.strictEqual(task.description, 'save the world')
+    assert.strictEqual(task.description, 'save the world');
+  })
+
+  it('should have a difficulty level', function () {
+    assert.strictEqual(task.difficulty, 10);
   })
 
 })
